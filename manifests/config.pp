7a36efde79f6c551e9d3c40d31623298a13a5de7
Nathan Spiegel <nspiegel@linode.com>
2021-10-28 06:47:54 -0400

    add port as default option

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,10 @@
 class node_exporter::config {
+  $options = hiera('node_exporter::options', '--web.listen-address=:9500')
   file { '/etc/default/node_exporter':
     ensure  => file,
     owner   => 'root',
     group   => 'root',
-    content => template('node_exporter/node_exporter.erb')
+    content => template('node_exporter/node_exporter.erb'),
+    notify  => Service['node_exporter']
   }
 }
