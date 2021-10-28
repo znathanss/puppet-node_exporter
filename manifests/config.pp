86eba550beae2299ba044c306a5dbf970a8888d4
Nathan Spiegel <nspiegel@linode.com>
2021-10-28 06:52:12 -0400

    and set port back to default after testing

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,5 +1,5 @@
 class node_exporter::config {
-  $options = hiera('node_exporter::options', '--web.listen-address=:9500')
+  $options = hiera('node_exporter::options', '--web.listen-address=:9100')
   file { '/etc/default/node_exporter':
     ensure  => file,
     owner   => 'root',
